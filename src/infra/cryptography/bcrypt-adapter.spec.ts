33984328fc0eed73a05921409d4216c2a0e2ea96
Lucas Matos Bognotti <lucasmatosbognotti@gmail.com>
2021-08-19 10:40:42 -0300

    refactor: move systemUnderTest creation to a factory helper method

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -7,19 +7,21 @@ jest.mock('bcrypt', () => ({
   }
 }))
 
+const salt = 12
+const makeSystemUnderTest = (): BCryptAdapter => {
+  return new BCryptAdapter(salt)
+}
+
 describe('BCrypt Adaptar', () => {
   test('Should call bcrypt with correct values', async () => {
-    const salt = 12
-    const systemUnderTest = new BCryptAdapter(salt)
+    const systemUnderTest = makeSystemUnderTest()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await systemUnderTest.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   test('Should return a hash on sucess', async () => {
-    const salt = 12
-    const systemUnderTest = new BCryptAdapter(salt)
-
+    const systemUnderTest = makeSystemUnderTest()
     const hash = await systemUnderTest.encrypt('any_value')
     expect(hash).toBe('hash')
   })
