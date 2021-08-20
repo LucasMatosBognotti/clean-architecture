e46883a6f90a63f49b456502274ae6f8b2feb303
Lucas Matos Bognotti <lucasmatosbognotti@gmail.com>
2021-08-19 23:48:53 -0300

    refactor: clean database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -12,6 +12,11 @@ describe('Account MongoDB Repository', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   const makeSystemUnderTest = (): AccountMongoRepository => {
     return new AccountMongoRepository()
   }
