c297a8565ebbf9a632644bd870a174c021f4d310
Lucas Matos Bognotti <lucasmatosbognotti@gmail.com>
2021-08-30 17:23:41 -0300

    refactor: move systemUnderTest creation to a factory method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -2,15 +2,33 @@ import { MissingParamError } from '../../errors'
 import { Validation } from './validation'
 import { ValidationComposite } from './validation-composite'
 
+interface SystemUnderTestTypes {
+  systemUnderTest: ValidationComposite
+  validationStub: Validation
+}
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error | undefined {
+      return undefined
+    }
+  }
+  return new ValidationStub()
+}
+
+const makeSystemUnderTest = (): SystemUnderTestTypes => {
+  const validationStub = makeValidation()
+  const systemUnderTest = new ValidationComposite([validationStub])
+  return {
+    systemUnderTest,
+    validationStub
+  }
+}
+
 describe('Validation Composite', () => {
   test('Should return an error if any validation fails', () => {
-    class ValidationStub implements Validation {
-      validate (input: any): Error {
-        return new MissingParamError('field')
-      }
-    }
-    const validationStub = new ValidationStub()
-    const systemUnderTest = new ValidationComposite([validationStub])
+    const { systemUnderTest, validationStub } = makeSystemUnderTest()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'))
     const error = systemUnderTest.validate({ field: 'any_value' })
     expect(error).toEqual(new MissingParamError('field'))
   })
