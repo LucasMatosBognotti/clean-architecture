9db7938f1f71f1c4863faa3c6779d7152fbb6ade
Lucas Matos Bognotti <lucasmatosbognotti@gmail.com>
2021-08-31 09:21:39 -0300

    refactor: change Authentication to receive a model

--- src/presentation/controllers/signin/signin.spec.ts
+++ src/presentation/controllers/signin/signin.spec.ts
@@ -1,6 +1,7 @@
 import { HttpRequest, Authentication, Validation } from './signin-protocols'
 import { MissingParamError, ServerError, UnathorizedError } from '../../errors'
 import { SignInController } from './signin'
+import { AuthenticationModel } from '../../../domain/usecases/authentication'
 
 interface SystemUnderTestTypes {
   systemUnderTest: SignInController
@@ -26,7 +27,7 @@ const makeValidation = (): Validation => {
 
 const makeAuthentication = (): Authentication => {
   class AuthenticationStub implements Authentication {
-    async auth (email: string, password: string): Promise<string> {
+    async auth (authentication: AuthenticationModel): Promise<string> {
       return new Promise(resolve => resolve('any_token'))
     }
   }
@@ -50,7 +51,7 @@ describe('SignIn Controller', () => {
     const { systemUnderTest, authenticationStub } = makeSystemUnderTest()
     const authSpy = jest.spyOn(authenticationStub, 'auth')
     await systemUnderTest.handle(makeFakeRequest())
-    expect(authSpy).toHaveBeenCalledWith('any_email@mail.com', 'any_password')
+    expect(authSpy).toHaveBeenCalledWith({ email: 'any_email@mail.com', password: 'any_password' })
   })
 
   test('Should return 401 if invalid credentials are provided', async () => {
