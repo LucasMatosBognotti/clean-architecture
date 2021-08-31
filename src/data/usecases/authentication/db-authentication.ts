c6edad0a67b52758f3cfcb09a5cfea402603b8b1
Lucas Matos Bognotti <lucasmatosbognotti@gmail.com>
2021-08-31 12:03:48 -0300

    feat: ensure DbAuthentication calls TokenGenerator with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,13 +1,16 @@
 import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
 import { HashComparer } from '../../protocols/cryptography/hash-comparer'
+import { TokenGenerator } from '../../protocols/cryptography/token-generator'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 
 export class DbAuthentication implements Authentication {
   private readonly hashComparer: HashComparer
+  private readonly tokenGenerator: TokenGenerator
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
 
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer) {
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer, tokenGenerator: TokenGenerator) {
     this.hashComparer = hashComparer
+    this.tokenGenerator = tokenGenerator
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
   }
 
@@ -15,6 +18,7 @@ export class DbAuthentication implements Authentication {
     const account = await this.loadAccountByEmailRepository.load(email)
     if (account) {
       await this.hashComparer.compare(password, account.password)
+      await this.tokenGenerator.generate(account.id)
     }
     return undefined
   }
