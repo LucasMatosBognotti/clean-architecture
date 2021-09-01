44d7afce4f52eb4c118696d4ed30fe30f5d3051f
Lucas Matos Bognotti <lucasmatosbognotti@gmail.com>
2021-09-01 11:14:10 -0300

    test: add signin route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,8 +1,12 @@
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 const MONGO_URL = process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-architecture'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -14,7 +18,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -27,6 +31,24 @@ describe('Login Routes', () => {
           email: 'lucasmatosbognotti@gmail.com',
           password: '123',
           passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /signin', () => {
+    test('Should return 200 on signin', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Lucas',
+        email: 'lucasmatosbognotti@gmail.com',
+        password
+      })
+      await request(app)
+        .post('/api/signin')
+        .send({
+          email: 'lucasmatosbognotti@gmail.com',
+          password: '123'
         })
         .expect(200)
     })
