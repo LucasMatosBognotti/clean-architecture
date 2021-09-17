19ec0a9d1e1a34ae0484ff04e11864d592f70e9a
lucasmatosbognotti <lucasmatosbognotti@gmail.com>
2021-09-17 13:03:50 -0300

    refactor: move systemUnderTest creation to a factory method

--- src/presentation/controllers/survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey-controller.spec.ts
@@ -1,6 +1,11 @@
 import { HttpRequest, Validation } from './add-survey-controller-protocols'
 import { AddSurveyController } from './add-survey-controller'
 
+interface SystemUnderTestTypes {
+  systemUnderTest: AddSurveyController
+  validationStub: Validation
+}
+
 const makeFakeRequest = (): HttpRequest => ({
   body: {
     question: 'any_question',
@@ -13,16 +18,28 @@ const makeFakeRequest = (): HttpRequest => ({
   }
 })
 
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error | undefined {
+      return undefined
+    }
+  }
+  return new ValidationStub()
+}
+
+const makeSystemUnderTest = (): SystemUnderTestTypes => {
+  const validationStub = makeValidation()
+  const systemUnderTest = new AddSurveyController(validationStub)
+  return {
+    systemUnderTest,
+    validationStub
+  }
+}
+
 describe('AddSuvey Controller', () => {
   test('Should call Validation with correct values', async () => {
-    class ValidationStub implements Validation {
-      validate (input: any): Error | undefined {
-        return undefined
-      }
-    }
-    const validationStub = new ValidationStub()
+    const { systemUnderTest, validationStub } = makeSystemUnderTest()
     const validateSpy = jest.spyOn(validationStub, 'validate')
-    const systemUnderTest = new AddSurveyController(validationStub)
     const httpRequest = makeFakeRequest()
     await systemUnderTest.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
