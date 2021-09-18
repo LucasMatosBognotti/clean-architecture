502967677514bfe71f5e900457cd0379de982c29
lucasmatosbognotti <lucasmatosbognotti@gmail.com>
2021-09-18 08:08:05 -0300

    refactor: move systemUnderTest creation to a factory method

--- src/data/usecases/add-survey/db-add-survey.spec.ts
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -1,6 +1,11 @@
 import { DbAddSurvey } from './db-add-survey'
 import { AddSurvey, AddSurveyRepository } from './db-add-survey-protocols'
 
+interface SystemUnderTestTypes {
+  systemUnderTest: DbAddSurvey
+  addSurveyRepositoryStub: AddSurveyRepository
+}
+
 const makeFakeSurveyData = (): AddSurvey.Params => ({
   question: 'any_question',
   answers: [{
@@ -9,16 +14,28 @@ const makeFakeSurveyData = (): AddSurvey.Params => ({
   }]
 })
 
+const makeAddSurveyRepository = (): AddSurveyRepository => {
+  class AddSurveyRepositoryStub implements AddSurveyRepository {
+    async add (data: AddSurveyRepository.Params): Promise<void> {
+      return new Promise(resolve => resolve())
+    }
+  }
+  return new AddSurveyRepositoryStub()
+}
+
+const makeSystemUnderTest = (): SystemUnderTestTypes => {
+  const addSurveyRepositoryStub = makeAddSurveyRepository()
+  const systemUnderTest = new DbAddSurvey(addSurveyRepositoryStub)
+  return {
+    systemUnderTest,
+    addSurveyRepositoryStub
+  }
+}
+
 describe('DbAddSurvey Usecase', () => {
   it('Should call AddSurveyRepository with correct values', async () => {
-    class AddSurveyRepositoryStub implements AddSurveyRepository {
-      async add (data: AddSurveyRepository.Params): Promise<void> {
-        return new Promise(resolve => resolve())
-      }
-    }
-    const addSurveyRepositoryStub = new AddSurveyRepositoryStub()
+    const { systemUnderTest, addSurveyRepositoryStub } = makeSystemUnderTest()
     const addSpy = jest.spyOn(addSurveyRepositoryStub, 'add')
-    const systemUnderTest = new DbAddSurvey(addSurveyRepositoryStub)
     await systemUnderTest.add(makeFakeSurveyData())
     expect(addSpy).toHaveBeenCalledWith(makeFakeSurveyData())
   })
