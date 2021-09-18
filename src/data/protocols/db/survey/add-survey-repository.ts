850be76cf93954e5b8023103f2da79c713ebf030
lucasmatosbognotti <lucasmatosbognotti@gmail.com>
2021-09-18 07:58:58 -0300

    feat: ensure DbAddSurvey calls AddSuveyRepository with correct values

---
+++ src/data/protocols/db/survey/add-survey-repository.ts
@@ -0,0 +1,9 @@
+import { AddSurvey } from '../../../../domain/usecases/add-survey'
+
+export interface AddSurveyRepository {
+  add: (data: AddSurveyRepository.Params) => Promise<void>
+}
+
+export namespace AddSurveyRepository {
+  export type Params = AddSurvey.Params
+}
---
+++ src/data/usecases/add-survey/db-add-survey.ts
@@ -0,0 +1,14 @@
+import { AddSurvey } from '../../../domain/usecases/add-survey'
+import { AddSurveyRepository } from './db-add-survey-protocols'
+
+export class DbAddSurvey implements AddSurvey {
+  private readonly addSurveyRepository: AddSurveyRepository
+
+  constructor (addSurveyRepository: AddSurveyRepository) {
+    this.addSurveyRepository = addSurveyRepository
+  }
+
+  async add (data: AddSurvey.Params): Promise<void> {
+    await this.addSurveyRepository.add(data)
+  }
+}
