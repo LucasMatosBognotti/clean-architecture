8b092819c1e491e28467d79fb4f52c3f1c620840
lucasmatosbognotti <lucasmatosbognotti@gmail.com>
2021-09-19 08:22:35 -0300

    refactor: move systemUnderTest creation to a factory method

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -3,28 +3,40 @@ import { AccessDeniedError } from '../errors'
 import { forbidden } from '../helpers/http/http-helper'
 import { AuthMiddleware } from './auth-middleware'
 
+interface SystemUnderTestTypes {
+  systemUnderTest: AuthMiddleware
+  loadAccountByTokenStub: LoadAccountByToken
+}
+
+const makeLoadAccountByToken = (): LoadAccountByToken => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load (accessToken: string, role?: string): Promise<LoadAccountByToken.Result> {
+      return new Promise(resolve => resolve({ id: 'any_id' }))
+    }
+  }
+
+  return new LoadAccountByTokenStub()
+}
+
+const makeSystemUnderTest = (): SystemUnderTestTypes => {
+  const loadAccountByTokenStub = makeLoadAccountByToken()
+  const systemUnderTest = new AuthMiddleware(loadAccountByTokenStub)
+  return {
+    systemUnderTest,
+    loadAccountByTokenStub
+  }
+}
+
 describe('Auth Middleware', () => {
   test('Should return 403 if no x-access-token exits in headers', async () => {
-    class LoadAccountByTokenStub implements LoadAccountByToken {
-      async load (accessToken: string, role?: string): Promise<LoadAccountByToken.Result> {
-        return new Promise(resolve => resolve({ id: 'any_id' }))
-      }
-    }
-    const loadAccountByTokenStub = new LoadAccountByTokenStub()
-    const systemUnderTest = new AuthMiddleware(loadAccountByTokenStub)
+    const { systemUnderTest } = makeSystemUnderTest()
     const httpResponse = await systemUnderTest.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
   })
 
   test('Should call loadAccountByToken with correct accessToken', async () => {
-    class LoadAccountByTokenStub implements LoadAccountByToken {
-      async load (accessToken: string, role?: string): Promise<LoadAccountByToken.Result> {
-        return new Promise(resolve => resolve({ id: 'any_id' }))
-      }
-    }
-    const loadAccountByTokenStub = new LoadAccountByTokenStub()
+    const { systemUnderTest, loadAccountByTokenStub } = makeSystemUnderTest()
     const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
-    const systemUnderTest = new AuthMiddleware(loadAccountByTokenStub)
     await systemUnderTest.handle({
       headers: {
         'x-access-token': 'any_token'
