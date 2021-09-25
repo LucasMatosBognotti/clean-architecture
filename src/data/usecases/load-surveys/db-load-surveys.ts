c0259c326383066924e0f8972a6e9663b6a4db17
lucasmatosbognotti <lucasmatosbognotti@gmail.com>
2021-09-25 11:11:45 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -0,0 +1,14 @@
+import { LoadSurveys } from '../../../domain/usecases/load-surveys'
+import { LoadSurveysRepository } from '../../protocols/db/survey/load-surveys-repository'
+
+export class DbLoadSurveys implements LoadSurveys {
+  private readonly loadSurveysRepository: LoadSurveysRepository
+
+  constructor (loadSurveysRepository: LoadSurveysRepository) {
+    this.loadSurveysRepository = loadSurveysRepository
+  }
+
+  async load (accountId: string): Promise<LoadSurveys.Result> {
+    return await this.loadSurveysRepository.loadAll(accountId)
+  }
+}
