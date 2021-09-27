f93c088b6a6a7900a120de5d7330a90acac8ad75
lucasmatosbognotti <lucasmatosbognotti@gmail.com>
2021-09-27 20:23:39 -0300

    feat: ensure SaveSurveyResulController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result-controller.ts
@@ -1,29 +1,39 @@
-import { Controller, LoadSurveyById, HttpResponse } from './save-survey-result-controller-protocols'
+import { Controller, LoadSurveyById, HttpResponse, SaveSurveyResult } from './save-survey-result-controller-protocols'
 import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 import { InvalidParamError } from '@/presentation/errors'
 
 export class SaveSurveyResultController implements Controller {
   private readonly loadSurveyById: LoadSurveyById
+  private readonly saveSurveyResult: SaveSurveyResult
 
-  constructor (loadSurveyById: LoadSurveyById) {
+  constructor (loadSurveyById: LoadSurveyById, saveSurveyResult: SaveSurveyResult) {
     this.loadSurveyById = loadSurveyById
+    this.saveSurveyResult = saveSurveyResult
   }
 
   async handle (request: SaveSurveyResultController.Request): Promise<HttpResponse> {
     try {
-      const { surveyId, answer } = request
+      const { surveyId, accountId, answer } = request
+
       const survey = await this.loadSurveyById.loadById(surveyId)
-      if (survey) {
-        const ansewers = survey.answers.map(a => a.answer)
-        if (!ansewers.includes(answer)) {
-          return forbidden(new InvalidParamError('answer'))
-        }
-      } else {
+      if (!survey) {
         return forbidden(new InvalidParamError('surveyId'))
       }
+
+      const answers = survey.answers.map(a => a.answer)
+      if (!answers.includes(answer)) {
+        return forbidden(new InvalidParamError('answer'))
+      }
+
+      await this.saveSurveyResult.save({
+        accountId,
+        surveyId,
+        answer,
+        date: new Date()
+      })
       return {
-        body: '',
-        statusCode: 200
+        statusCode: 200,
+        body: ''
       }
     } catch (err) {
       return serverError(err as Error)
