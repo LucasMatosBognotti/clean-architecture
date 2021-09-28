7a480471faee7b6d6bfebb866cbb74d8c9bde2a8
lucasmatosbognotti <lucasmatosbognotti@gmail.com>
2021-09-28 11:02:07 -0300

    test: ensure save survey result route return 403 if no token is provided

---
+++ src/main/routes/survey-result-routes.test.ts
@@ -0,0 +1,36 @@
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import app from '../config/app'
+
+const MONGO_URL = process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-architecture'
+
+let surveyCollection: Collection
+let accountCollection: Collection
+
+describe('Survey Result Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('PUT /surveys/:surveyId/results', () => {
+    it('Should return 403 on save survey result without accessToken', async () => {
+      await request(app)
+        .put('/api/surveys/any_id/results')
+        .send({ answer: 'any_answer' })
+        .expect(403)
+    })
+  })
+})
