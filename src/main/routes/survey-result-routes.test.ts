ccb599486ea0ed19c2aac28c352ba3d620a9c7dc
lucasmatosbognotti <lucasmatosbognotti@gmail.com>
2021-09-28 11:43:22 -0300

    test: ensure save survey result route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,12 +1,28 @@
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
 import app from '../config/app'
+import env from '../config/env'
 
 const MONGO_URL = process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-architecture'
 
 let surveyCollection: Collection
 let accountCollection: Collection
+
+const makeAccessToken = async (): Promise<string> => {
+  const response = await accountCollection.insertOne({
+    name: 'Lucas',
+    email: 'lucasmatosbognotti@gmail.com',
+    password: '123'
+  })
+  const id = response.ops[0]._id
+  const accessToken = sign({ id }, env.JWT_SECRET)
+  await accountCollection.updateOne({
+    _id: id
+  }, { $set: { accessToken } })
+  return accessToken
+}
 
 describe('Survey Result Routes', () => {
   beforeAll(async () => {
@@ -31,6 +47,28 @@ describe('Survey Result Routes', () => {
         .put('/api/surveys/any_id/results')
         .send({ answer: 'any_answer' })
         .expect(403)
+    })
+
+    it('Should return 200 on add survey with valid accessToken', async () => {
+      const accessToken = await makeAccessToken()
+      const response = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+
+      await request(app)
+        .put(`/api/surveys/${response.ops[0]._id}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'Answer 1'
+        })
+        .expect(200)
     })
   })
 })
